671f34803ce9fcf848e63067f6afe0b42980433f
Mateusz Świetlik <swiecium@gmail.com>
2023-11-20 18:33:51 +0100

    first commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+for (const image of galleryItems) {
+  const item = `<li>
+    <a class="gallery__item" href="${image.original}">
+        <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+    </a>
+</li>`;
+  gallery.insertAdjacentHTML('beforeend', item);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
