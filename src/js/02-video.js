82e78387955e2b912a78c0f933d98f9d04544520
Mateusz Świetlik <swiecium@gmail.com>
2023-12-07 19:39:17 +0100

    update video2.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,6 +3,7 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+
 player.on(
   'timeupdate',
   throttle(event => {
@@ -12,5 +13,7 @@ player.on(
 
 window.addEventListener('DOMContentLoaded', () => {
   const time = localStorage.getItem('videoplayer-current-time');
-  player.setCurrentTime(time);
+  if (time !== null) {
+    player.setCurrentTime(time);
+  }
 });
